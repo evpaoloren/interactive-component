5ea74ea1ed210eb1ad154c2e712d4cc44d51eb43
Paolo Ren Ebueza <preebueza@gmail.com>
2023-01-22 13:29:13 +0800

    completed project

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const submit = document.querySelector('#submitButton');
+const ratingContainer = document.querySelector('.container-rating');
+const containerRated = document.querySelector('.container-rated');
+// const rated = document.querySelector('.rated')
 
 submit.addEventListener('click', function(e) {
     e.preventDefault();
-    console.log('Thank you for the rating');
+    if(containerRated.style.display !== 'none') {
+        containerRated.style.display = 'none';
+    } else {
+        containerRated.style.display = 'flex';
+        ratingContainer.style.display = 'none';
+        rated.textContent = displayRadioValue();
+    }
 });
+
+function displayRadioValue() {
+    const ele = document.getElementsByName('rating');
+      
+    for(i = 0; i < ele.length; i++) {
+        if(ele[i].checked)
+        return document.querySelector("#rated").textContent = " "+ele[i].value;
+    }
+}
